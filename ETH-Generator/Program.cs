8872c4434c547fdc2af1b27fbff24e34497df7cc
Akulav <43496076+Akulav@users.noreply.github.com>
2022-08-19 00:41:43 +0300

    Fixed Bugs + Refactor
    1. Fixed DB initialization bug
    2. Optimized DB connection
    3. Optimized Data Display
    4. Reduced thread delay

--- ETH-Generator/Program.cs
+++ ETH-Generator/Program.cs
@@ -5,81 +5,82 @@ using System.Data.SQLite;
 
 namespace ETH_Generator
 {
-    
+
     internal class Program
     {
         public static int index = 0;
         public static int guess = 0;
+        public static readonly int delay = 120;
+
         static void Main(string[] args)
         {
+            //Create the db if not created yet
+            InitializeDataSet();
+
+            //Open the connection to the database
             var con = new SQLiteConnection(Paths.database_connection);
             con.Open();
-            InitializeDataSet(con);
-            var web3 = new Web3("https://newest-powerful-mansion.discover.quiknode.pro/e3cbef755ee0582620a34cf99e49d9e20f79a353/");
+
+            //Make connection to the blockchain
+            var web3 = new Web3("YOUR WEB3 SERVER");
+
+            //Call the execution
             Generate(web3, con);
         }
 
-        static void Generate(Web3 web3, SQLiteConnection con)
+        static async void Generate(Web3 web3, SQLiteConnection con)
         {
+
             while (true)
             {
-                Thread thread = new(async() =>
+                Thread thread = new(async () =>
                 {
-
                     var mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
-                    //Console.WriteLine("The 12 seed words are: " + mnemonic.ToString());
-
                     var password = "password";
                     var wallet = new Wallet(mnemonic.ToString(), password);
                     var account = wallet.GetAccount(0);
 
                     var balance = await web3.Eth.GetBalance.SendRequestAsync(account.Address);
-                    //Console.WriteLine("Address at Index 0 is: " + account.Address + " with private key:" + account.PrivateKey);
-                    //Console.WriteLine("Balance of Ethereum Foundation's account in Ether: " + Web3.Convert.FromWei(balance.Value));
-                    if (Web3.Convert.FromWei(balance.Value) == 0) { Console.WriteLine("Verified " + index.ToString() + " Guessed " + guess.ToString()); }
-                    else {
+
+                    if (Web3.Convert.FromWei(balance.Value) != 0)
+                    {
                         guess++;
-                        Console.WriteLine("Verified " + index.ToString() + " Guessed " + guess.ToString());
+                        Console.WriteLine("FUCK YES");
                         var data_cmd = new SQLiteCommand(con)
                         {
                             CommandText = $@"INSERT INTO data(key, balance) VALUES ('{account.PrivateKey}', '{balance}')"
                         };
                         data_cmd.ExecuteNonQuery();
                     }
-
+                    Console.WriteLine("[Checked] " + index.ToString() + " \n[Guessed] " + guess.ToString() + " \n[Address] " + account.Address + " \n[Private] " + account.PrivateKey + " \n[Balance] " + balance + "\n----------------");
                     index++;
                 });
                 thread.Start();
-                Thread.Sleep(210);
+                Thread.Sleep(delay);
             }
         }
 
-        public static void InitializeDataSet(SQLiteConnection con)
+        public static void InitializeDataSet()
         {
-            try
+            if (!Directory.Exists(Paths.fileLocation))
             {
-                if (!Directory.Exists(Paths.fileLocation))
-                {
-                    Directory.CreateDirectory(Paths.fileLocation);
-                }
-
-                if (!File.Exists(Paths.database))
-                {
-                    File.WriteAllText(Paths.database, null);
-                    con.Open();
-                    var data_cmd = new SQLiteCommand(con)
-                    {
-                        CommandText = @"CREATE TABLE data(key VARCRHAR(250), balance VARCRHAR(250))"
-                    };
-
-                    data_cmd.ExecuteNonQuery();
-                    con.Close();
-                }
-
+                Directory.CreateDirectory(Paths.fileLocation);
             }
-            catch { }
-        }
 
+            if (!File.Exists(Paths.database))
+            {
+                var con = new SQLiteConnection(Paths.database_connection);
+                File.WriteAllText(Paths.database, null);
+                con.Open();
+                var data_cmd = new SQLiteCommand(con)
+                {
+                    CommandText = @"CREATE TABLE data(key VARCRHAR(250), balance VARCRHAR(250))"
+                };
+
+                data_cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
     }
 }
 
