799f62b0fdc047dfcb5c3a3388aafa14f25bf481
Akulav <43496076+Akulav@users.noreply.github.com>
2022-08-19 05:13:39 +0300

    Add elapsed time

--- ETH-Generator/Program.cs
+++ ETH-Generator/Program.cs
@@ -1,7 +1,9 @@
 ﻿using NBitcoin;
 using Nethereum.HdWallet;
 using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
 using System.Data.SQLite;
+using System.Diagnostics;
 
 namespace ETH_Generator
 {
@@ -10,9 +12,9 @@ namespace ETH_Generator
     {
         public static int index = 0;
         public static int guess = 0;
-        public static readonly int delay = 120;
+        public static readonly int delay = 80;
 
-        static void Main(string[] args)
+        static void Main()
         {
             //Create the db if not created yet
             InitializeDataSet();
@@ -22,23 +24,27 @@ namespace ETH_Generator
             con.Open();
 
             //Make connection to the blockchain
-            var web3 = new Web3("YOUR WEB3 SERVER");
+            var web3 = new Web3("https://newest-powerful-mansion.discover.quiknode.pro/e3cbef755ee0582620a34cf99e49d9e20f79a353/");
+
+            //Start clock
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
 
             //Call the execution
-            Generate(web3, con);
+            Generate(web3, con, stopwatch);
         }
 
-        static async void Generate(Web3 web3, SQLiteConnection con)
+        static async void Generate(Web3 web3, SQLiteConnection con, Stopwatch watch)
         {
 
             while (true)
             {
                 Thread thread = new(async () =>
                 {
-                    var mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
-                    var password = "password";
-                    var wallet = new Wallet(mnemonic.ToString(), password);
-                    var account = wallet.GetAccount(0);
+                    Mnemonic mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
+                    string password = "";
+                    Wallet wallet = new Wallet(mnemonic.ToString(), password);
+                    Account account = wallet.GetAccount(0);
 
                     var balance = await web3.Eth.GetBalance.SendRequestAsync(account.Address);
 
@@ -52,7 +58,7 @@ namespace ETH_Generator
                         };
                         data_cmd.ExecuteNonQuery();
                     }
-                    Console.WriteLine("[Checked] " + index.ToString() + " \n[Guessed] " + guess.ToString() + " \n[Address] " + account.Address + " \n[Private] " + account.PrivateKey + " \n[Balance] " + balance + "\n----------------");
+                    Console.WriteLine("[Elapsed] " + watch.Elapsed + " \n[Checked] " + index.ToString() + " \n[Guessed] " + guess.ToString() + " \n[Address] " + account.Address + " \n[Private] " + account.PrivateKey + " \n[Balance] " + balance + "\n----------------");
                     index++;
                 });
                 thread.Start();
